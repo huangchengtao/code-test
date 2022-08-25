ce51bb039d187584e39b194346aa833fb62eee69
黄成涛-合力-业务支撑 <huangct@hollycrm.com>
2022-08-25 13:42:17 +0800

    测试泄露扫描

---
+++ src/main/java/com/uchonor/CodeScanCheck.java
@@ -0,0 +1,12 @@
+package com.uchonor;
+
+public class CodeScanCheck {
+
+    public static void main(String[] args) {
+        System.out.println("----->>uchonor");
+        System.out.println("----->>com.unicom.microserv.cs");
+        System.out.println("----->>com.uchonor");
+        System.out.println("----->>ucbeacons");
+        System.out.println("----->>ucbeacon");
+    }
+}
