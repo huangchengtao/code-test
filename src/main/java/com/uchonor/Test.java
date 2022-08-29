0fe8b9251ed67b7ff658429882e28d611ba682ca
huangchengtao <39635552@qq.com>
2022-08-30 05:39:09 +0800

    Update Test.java

--- src/main/java/com/uchonor/Test.java
+++ src/main/java/com/uchonor/Test.java
@@ -2,6 +2,10 @@ package com.uchonor;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("111111111111");
+         System.out.println("----->>uchonor");
+        System.out.println("----->>com.unicom.microserv.cs");
+        System.out.println("----->>com.uchonor");
+        System.out.println("----->>ucbeacons");
+        System.out.println("----->>ucbeacon");
     }
 }
